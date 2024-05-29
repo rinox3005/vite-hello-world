525fbb7ce72b6ebb7abab210b718ce76002b9e53
rinox3005 <peppe.rinox@gmail.com>
2024-05-29 17:19:57 +0200

    [UPD] created AppTitle component to show a message on page, styled

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
-<script></script>
-<template></template>
+<script>
+import AppTitle from "./components/AppTitleComponent.vue";
+export default {
+  name: "MyApp",
+  data() {
+    return {
+      message: "Hello World",
+    };
+  },
+  components: {
+    AppTitle,
+  },
+};
+</script>
+
+<template>
+  <main>
+    <AppTitle />
+  </main>
+</template>
+
 <style></style>
---
+++ src/components/AppTitleComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "AppTitle",
+};
+</script>
+
+<template>
+  <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style>
+h1 {
+  text-shadow: #fff 1px 0 10px;
+}
+</style>
